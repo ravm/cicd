951e5a4b6e9971bb1f4b5d3457ef194c55567368
aherman21 <hermansalexander2@gmail.com>
2024-04-01 11:29:53 +0300

    daystohours app

--- daystohours/src/App.jsx
+++ daystohours/src/App.jsx
@@ -1,30 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  function daysToHoursConverter(days) {
+    return days * 24
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h2>Days to Hours Converter</h2>
+        <p>Enter the number of days to convert to hours</p>
+        <input
+          type="number"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {count} day(s) is equal to {daysToHoursConverter(count)} hour(s)
         </p>
-      </div>
+        </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>

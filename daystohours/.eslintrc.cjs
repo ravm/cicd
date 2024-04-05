a3151578960ecdb6d0ddd7528867df4c1fdd6669
aherman21 <hermansalexander2@gmail.com>
2024-04-05 14:42:30 +0300

    eslint configuration modified

---
+++ daystohours/.eslintrc.cjs
@@ -0,0 +1,47 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': false,
+		'es2021': true,
+		'node': true
+	},
+	'plugins': [
+		'@stylistic/js'
+	],
+	'extends': 'eslint:recommended',
+	'overrides': [
+		{
+			'env': {
+				'node': true
+			},
+			'files': [
+				'.eslintrc.{js,cjs}'
+			],
+			'parserOptions': {
+				'sourceType': 'script'
+			}
+		}
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'rules': {
+		'@stylistic/js/indent': [
+			'error',
+			'tab'
+		],
+		'@stylistic/js/linebreak-style': [
+			'error',
+			'unix'
+		],
+		'@stylistic/js/quotes': [
+			'error',
+			'single'
+		],
+		'@stylistic/js/semi': [
+			'error',
+			'never'
+		]
+	}
+}
--- daystohours/src/App.jsx
+++ daystohours/src/App.jsx
@@ -24,7 +24,7 @@ function App () {
         </p>
         </div>
     </>
-  );
+  )
 }
 
 export default App;

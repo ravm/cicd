1690d2111e4540dacc8c00bdcb0c75dae5228894
ravm <miikaravi99@gmail.com>
2024-04-08 13:28:07 +0300

    added links to github and codecov

--- daystohours/src/App.jsx
+++ daystohours/src/App.jsx
@@ -11,7 +11,7 @@ function App () {
 	return (
 		<>
 			<div className="card">
-				<h2>Days to hours converter</h2>
+				<h2>Days to Hours Converter</h2>
 				<p>Enter the number of days to convert to hours</p>
 				<input
 					type="number"
@@ -24,6 +24,16 @@ function App () {
 						{count} day(s) is equal to {Number.isInteger(((Number(count)))) ? count * 24 : (Number(daysToHoursConverter(count)).toFixed(2))} hour(s)
 					</p>
 				)}
+				<p>
+					<a href='https://github.com/ravm/cicd'>
+						Github repository
+					</a>
+				</p>
+				<p>
+					<a href='https://app.codecov.io/github/ravm/cicd'>
+						Coverage
+					</a>
+				</p>
 			</div>
 		</>
 	)
